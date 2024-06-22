ba8b29d8df2349ea53aeab6914bde1cc49e85b1d
Kisonam <kisonamplay@gmail.com>
2024-06-22 14:45:06 +0200

    End of section 8

--- Angular-.NET/client/src/app/app.component.ts
+++ Angular-.NET/client/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { Product } from './models/product';
+import { Pagination } from './models/pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Client App';
+export class AppComponent implements OnInit {
+  title = 'Client';
+  products: Product[] = [];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>('https://localhost:5001/api/products?pageSize=50').subscribe({
+      next: response  => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('Request Completed')
+    })
+  }
 }
---
+++ Angular-.NET/client/src/app/models/product.ts
@@ -0,0 +1,9 @@
+export interface Product {
+    id:           number;
+    name:         string;
+    description:  string;
+    pictureUrl:   string;
+    price:        number;
+    productType:  string;
+    productBrand: string;
+}
